e627bf3d89b699818ed171e93ceb7359975132c0
jianshentan <jianshentan@gmail.com>
2014-11-15 13:04:43 -0500

    email

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,4 @@
-var nodemailer = require('nodemailer');
-
-// create reusable transporter object using SMTP transport
-var transporter = nodemailer.createTransport("SMTP", {
-    service: 'Gmail',
-    auth: {
-        user: 'themcmassproject@gmail.com',
-        pass: 'mik3yDunc#'
-    }
-});
-
-var mailOptions = {
-    from: 'McMass ✔ <themcmassproject@gmail.com>', // sender address
-    to: 'themcmassproject@gmail.com', // list of receivers
-    subject: 'NEW SUBSCRIBER ✔', // Subject line
-    text: 'Hello world ✔', // plaintext body
-    html: '' // html body
-};
+var emails = [];
 
 exports.index = function(req, res){
     res.render('index');
@@ -23,21 +6,8 @@ exports.index = function(req, res){
 
 exports.email = function(req, res){
     var email =  req.query.email; 
-    console.log( email );
-
-    mailOptions.subject = "NEW SUBSCRIBER: " + email;
-    mailOptions.text = email;
-
+    emails.push( email );
     res.send( 200 );
-    /*
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Message sent: ' + info.response);
-        }
-    });
-    */
 };
 
 exports.faq = function(req, res){
@@ -46,6 +16,10 @@ exports.faq = function(req, res){
 
 exports.shop = function(req, res){
     res.render('shop');
+};
+
+exports.subscribers = function(req, res) {
+    res.render('subscribers', { subscribers: emails });
 };
 
 
