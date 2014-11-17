b2747aa6197c3a3239165e36eef04669edcf4a31
jianshentan <jianshentan@gmail.com>
2014-11-16 19:32:12 -0500

    email

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
+var fs = require( 'fs' ),
+    api_key = fs.readFileSync( 'send_grid_api_key', 'utf8' ),
+    api_key = api_key.replace(/(\r\n|\n|\r)/gm,"")
+    sendgrid  = require('sendgrid')("mcmass", api_key);
+console.log( sendgrid );
+
 var emails = [];
 
 exports.index = function(req, res){
@@ -7,6 +13,20 @@ exports.index = function(req, res){
 exports.email = function(req, res){
     var email =  req.query.email; 
     emails.push( email );
+
+    var sgEmail = new sendgrid.Email({
+        to:       email,
+        bcc:      'themcmassproject@gmail.com',
+        from:     'themcmassproject@gmail.com',
+        subject:  "Thanks for Subscribing to the McMass Campaign: " + email,
+        text:     "Thanks for subscribing! \n We'll keep you posted with only our most exciting updates! \n"
+    });
+
+    sendgrid.send(sgEmail, function(err, json) {
+        if (err) { return console.error(err); }
+            console.log(json);
+    });
+
     res.send( 200 );
 };
 
@@ -21,6 +41,4 @@ exports.shop = function(req, res){
 exports.subscribers = function(req, res) {
     res.render('subscribers', { subscribers: emails });
 };
-
-
 
